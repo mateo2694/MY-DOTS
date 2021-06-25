f69cdcf7853012df61bd7fe5aea2cdd2c8b747da
mateo2694 <mateosm@outlook.com>
2021-06-24 21:56:36 -0500

    Completed basic page structure, added some simple graphs and completed heatmap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header.js';
+import Navbar from './Navbar.js';
+import Footer from './Footer.js';
+import { BarPlot, ScatterPlot, BubbleChart, HistogramPlot, Heatmap } from './CustomPlot.js';
+import data from './usageData.js';
+
+// week days
+const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+// Data
+const timestamp = data.map(chunk => chunk['timestamp']);
+const usage = data.map(chunk => chunk['utilizacion-turno-porcentual'].toFixed(1));
+const date = timestamp.map(timestamp => new Date(timestamp));
+const days = date.map(date => weekDays[date.getDay()]);
+
+const dailyUsage = new Array(weekDays.length);
+
+weekDays.forEach((day, index) => {
+  dailyUsage[index] = usage.filter(function (usage, index) {
+    return this[index] === day ? usage : 0;
+  }, days);
+});
+
+const MIN_USAGE = Math.min(...usage);
+const MAX_USAGE = Math.max(...usage);
+const NUM_RANGES = 20;
+
+let limits = new Array(NUM_RANGES + 1).fill(0).map((value, index) => {
+  return value += (MAX_USAGE - MIN_USAGE) / NUM_RANGES * index + MIN_USAGE;
+});
+
+// fix
+limits[0] -= 0.1;
+limits[NUM_RANGES] += 0.1;
+
+let heatmapUsage = new Array(NUM_RANGES).fill(null).map(value => {
+  return new Array(weekDays.length).fill(null);
+});
+
+dailyUsage.forEach((usage, col) => {
+  limits.forEach((limit, row) => {
+    if (row === 0) {
+      return;
+    }
+
+    const count = usage.filter(usage => usage > limits[row - 1] && usage <= limit).length;
+    if (count > 0) {
+      heatmapUsage[row - 1][col] = count;
+    }
+
+  });
+});
+
+
+console.log(dailyUsage);
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Navbar></Navbar>
+      <BarPlot date={date} usage={usage}></BarPlot>
+      <ScatterPlot date={date} usage={usage}></ScatterPlot>
+      <BubbleChart date={date} usage={usage}></BubbleChart>
+      <HistogramPlot date={date} usage={usage}></HistogramPlot>
+      <Heatmap x={weekDays} y={limits} z={heatmapUsage}></Heatmap>
+      <Footer></Footer>
     </div>
   );
 }
