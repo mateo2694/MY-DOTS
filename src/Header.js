f047f6f6cc52d41ac68de0a420a610bcce0b2418
mateo2694 <mateosm@outlook.com>
2021-06-26 00:55:46 -0500

    Completed responsive features

--- src/Header.js
+++ src/Header.js
@@ -5,23 +5,65 @@ import userIcon from './img/icon-user.png';
 import arrowIcon from './img/icon-arrow-down.png';
 
 
-function Header(props) {
-    return (
-        <header className='header lateral-padding'>
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isActive: false
+        }
+        this.wasMounted = false;
+        this.handleHamburgerClick = this.handleHamburgerClick.bind(this);
+    }
+
+    handleHamburgerClick() {
+        this.setState({ isActive: !this.state.isActive });
+    }
+
+    componentDidMount() {
+        this.wasMounted = true;
+    }
+
+    render() {
+        let listMobile = null;
+
+        if (this.wasMounted) {
+            listMobile = (
+                <ul className={'header__list--mobile text--emphasis ' +
+                    (this.state.isActive ? 'expand-animation' : 'contract-animation')}>
+                    <li className='header__item--mobile'>Dashboard</li>
+                </ul>
+            );
+        }
+
+        return (
             <div>
-                <img className='header__logo' src={logo} alt='Logo' />
-                <span className='header__title text--emphasis'>My Dots</span>
+                <header className='header lateral-padding'>
+                    <div>
+                        <img className='header__logo' src={logo} alt='Logo' />
+                        <span className='header__title text--emphasis'>My Dots</span>
+                    </div>
+                    <ul className='header__list text--emphasis'>
+                        <li className='header__item'>Dashboard</li>
+                    </ul>
+                    <div>
+                        <div className='header__user'>
+                            <img className='user__picture' src={userIcon} alt='User'></img>
+                            <img className='user__arrow' src={arrowIcon} alt='Arrow'></img>
+                            <p className='user__name text--normal'>User:<br />Mateo Sánchez</p>
+                        </div>
+                        <div className={'header__hamburger ' +
+                            (this.state.isActive ? 'hamburger--active' : '')}
+                            onClick={this.handleHamburgerClick}>
+                            <div className='hamburger__slice slice--top' />
+                            <div className='hamburger__slice slice--middle' />
+                            <div className='hamburger__slice slice--bottom' />
+                        </div>
+                    </div>
+                </header>
+                {listMobile}
             </div>
-            <ul className='header__list text--emphasis'>
-                <li className='header__item'>Dashboard</li>
-            </ul>
-            <div className='header__user'>
-                <img className='user__picture' src={userIcon} alt='User'></img>
-                <img className='user__arrow' src={arrowIcon} alt='Arrow'></img>
-                <p className='user__name text--normal'>User:<br />Mateo Sánchez</p>
-            </div>
-        </header>
-    );
+        );
+    }
 }
 
 export default Header;
